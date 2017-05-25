6f073fc50071065814e060db01637a592b525dc4
Zenith Wogwugwu <zenith@kraneum.com>
2017-05-25 10:44:45 +0100

    no message

--- public_html/js/map.js
+++ public_html/js/map.js
@@ -14,17 +14,19 @@ $(function () {
         zoom: 15
     };
     var vars = {
+        map:null,
+        myMarker:null,
         googleMaps: null,
-        myLoc: null, //{lat: -34.397, lng: 150.644}
-        myPos: null,//full position information returned by geolocation api
-        mapElem :document.getElementById('map')
+        myLoc: {}, //{lat: -34.397, lng: 150.644}
+        myPos: {}, //full position information returned by geolocation api
+        mapElem: document.getElementById('map')
     };
 
     //init
-    //get googleMaps
+    //get google.maps
     $(function _() {
         setTimeout(function () {
-            if (!(google && google.maps)) {
+            if (typeof google !== 'object' || !google.maps) {
                 return _();
             }
 
@@ -34,26 +36,37 @@ $(function () {
     });
 
     function init() {
-        console.log('Init');
         getMyLoc();
     }
     function getMyLoc() {
         navigator.geolocation.watchPosition(getMyLocSuccess, getMyLocError, {enableHighAccuracy: true, maximumAge: 30000, timeout: 27000});
     }
     function getMyLocSuccess(pos) {
+        !vars.map && (vars.map = new vars.googleMaps.Map(vars.mapElem, {
+            center: {lat: pos.coords.latitude, lng: pos.coords.longitude},
+            zoom: config.zoom, //set other map options, i.e wen dnt want default controls to show on d map, and we want to set handlers for when d person clicks or scrolls the map
+            disableDefaultUI: true
+        }));
+        
+        if(pos.coords.latitude === vars.myLoc.lat && pos.coords.longitude === vars.myLoc.lng){
+            return;
+        }
+        
+        console.log(pos.coords.accuracy);
         //if the accuracy is too low, info the person that he's gps accuracy is low and he should select he's current position
-        if (pos.coords.accuracy <= vars.minAccuracy) {
-            alert('You gps accuracy is too low, please select your current location from the map');
+        if (pos.coords.accuracy > config.minAccuracy) {
+            alert('Your gps accuracy is too low, please select your current location from the map');
         }
 
         vars.myPos = pos;
         //maybe if u are moving fast(Max/min speed), i can adjust(reduce) the maximum age of the watchPosition and if u slow down, i'll adjust(increase) the maximum age again
         //i dnt kw wht to do with altitude infomation, u dey fly ni, go use another app :-D
-        refreshMap(vars.myLoc = {lat: pos.coords.latitude, lng: pos.coords.longitude});
+        vars.myLoc = {lat: pos.coords.latitude, lng: pos.coords.longitude};
+        updateMyLocation();
     }
     function getMyLocError(err) {
-        //maybe on error, if google maps has nt initialised , check server or local storage and get the last location d user was and display it in the map, and tell the user to turn on location or select hes location on d map
-        
+        //maybe on error, if google maps has nt initialised , check server or local storage and get the last location d user was and display it in the map, or if u hv nt used d app bfr, then it'll use ur ip address to determine ur location and display that location, then also tell the user to turn on location or select hes location on d map
+
         console.error('Get location err: ' + JSON.stringify(err));
 
         switch (err.code) {
@@ -65,15 +78,21 @@ $(function () {
         alert('Problem getting your current location, please check if your location is switched on');
     }
 
-    function refreshMap(locs) {
-        var map = new vars.googleMaps.Map(vars.mapElem, {
-            center: locs,
-            zoom: config.zoom
-        }), marker = new vars.googleMaps.Marker({
-            position: locs,
-            map: map
-        });
+    function updateMyLocation() {
+        //lol did we really do this?
+        //to prevent the location marker from having d blinking effect, first save the reference to the old marker, put the new marker, then delete the old marker
+        !vars.myMarker && (vars.myMarker = new vars.googleMaps.Marker({
+            position: vars.myLoc,
+            map: vars.map,
+            title:'me'
+        }));
+        
+        vars.myMarker.setPosition(vars.myLoc);
     }
-
+    
+    function updateLocations(locs){
+        //lol did we really do this?
+        //to prevent the location marker from having d blinking effect, first save the reference to the old marker, put the new marker, then delete the old marker
+    }
 
 });
