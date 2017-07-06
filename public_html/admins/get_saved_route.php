d0bf2c55f8ec4eeff4b1a5d02dc3f63474da72f6
Zenith Wogwugwu <zenith@kraneum.com>
2017-07-06 05:18:26 +0100

    no message

--- public_html/admins/get_saved_route.php
+++ public_html/admins/get_saved_route.php
@@ -13,10 +13,13 @@ $routeId = filter_input(INPUT_POST, 'i', FILTER_SANITIZE_STRING);
 if ($routeId) {
     $r = $mongoDB->executeQuery('bustops.routes', new MongoDB\Driver\Query(['admin_id' => $adminId, '_id' => new MongoDB\BSON\ObjectID($routeId)], ['projection' => ['_id' => 0, 'admin_id' => 0], 'limit' => 1]))->toArray()[0];
     $r->nstops = [];
+    $n;
     foreach ($r->stops as $i) {
-        $r->nstops[] = $mongoDB->executeQuery('bustops.locations', new MongoDB\Driver\Query(['_id' => new MongoDB\BSON\ObjectID($i)], ['projection' => ['_id' => 0, 'names' => 1], 'limit' => 1]))->toArray()[0]->names;
+        $n = $mongoDB->executeQuery('bustops.locations', new MongoDB\Driver\Query(['_id' => new MongoDB\BSON\ObjectID($i)], ['projection' => ['_id' => 0, 'names' => 1], 'limit' => 1]))->toArray();
+        $r->nstops[] = isset($n[0]) ? $n[0]->names : [];
     }
-    $r->nhub = $mongoDB->executeQuery('bustops.locations', new MongoDB\Driver\Query(['_id' => new MongoDB\BSON\ObjectID($r->hub)], ['projection' => ['_id' => 0, 'names' => 1], 'limit' => 1]))->toArray()[0]->names;
+    $n = $mongoDB->executeQuery('bustops.locations', new MongoDB\Driver\Query(['_id' => new MongoDB\BSON\ObjectID($r->hub)], ['projection' => ['_id' => 0, 'names' => 1], 'limit' => 1]))->toArray();
+    $r->nhub = isset($n[0]) ? $n[0]->names : [];
 
     echo json_encode($r);
 }
