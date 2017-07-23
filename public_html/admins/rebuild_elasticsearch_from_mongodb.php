f8fa767c1de9eb117c2c4ace55635bd1944f898f
zenithy <wogwugwu.zenith@gmail.com>
2017-07-23 02:19:32 +0100

    no message

--- public_html/admins/rebuild_elasticsearch_from_mongodb.php
+++ public_html/admins/rebuild_elasticsearch_from_mongodb.php
@@ -8,6 +8,8 @@
 require_once '../php/mongodb.php';
 require_once '../php/elasticsearch_client.php';
 
+ini_set('max_execution_time', 420);
+
 foreach ($mongoDB->executeQuery('bustops.locations', new MongoDB\Driver\Query([], ['projection' => ['names' => 1, 'addresses' => 1]])) as $r) {
     $elasticresult = $elasticsearchClient->index([
         'body' => ['names' => $r->names, 'addresses' => $r->addresses],
